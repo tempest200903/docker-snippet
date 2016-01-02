47f3dd3d857fc83df6569503ed262281bf6aa633
tempest200903 <tempest200903@gmail.com>
2016-01-02 20:57:10 +0900

    Update install-docker-registry.sh

--- install-docker-registry.sh
+++ install-docker-registry.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -uex
 #:name: install-docker-registry.sh
-#:usage: curl installhttps://raw.githubusercontent.com/tempest200903/docker-snippet/master/install-docker-registry.sh | sh
+#:usage: curl https://raw.githubusercontent.com/tempest200903/docker-snippet/master/install-docker-registry.sh | sh
 yum -y install epel-release
 yum -y install docker-registry python-sqlalchemy
 ls /etc/sysconfig/docker-registry
